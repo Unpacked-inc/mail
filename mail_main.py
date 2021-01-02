e19c9d09ed513be9e23e5fb0cd5e981748d6e880
【yuuki00682200】 <yuuki_19n1100530@nnn.ed.jp>
2021-01-02 10:56:43 +0900

    メアドを作成する関数を作成

--- mail_main.py
+++ mail_main.py
@@ -1,2 +1,17 @@
 #捨てアドレスを大量に取得する
 
+#必要なモジュール
+from bs4 import BeautifulSoup
+import requests
+import re
+
+#正規表現でタグを外す(スクレイピングで余計なものまでついてきてしまってイライラするので)
+p = re.compile(r"<[^>]*?>")
+
+#メールアドレスを生成する関数
+def createMail():
+    payload = {
+    'account_member[login]':"
+    }
+    '
+
